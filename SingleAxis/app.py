171578b274827c84546396c0f9a40433b7ae9744
Thomas Dalla Piazza <thomas.dallapiazza@gmail.com>
2023-10-27 15:58:21 +0200

    Added WebApp

---
+++ SingleAxis/app.py
@@ -0,0 +1,33 @@
+import os
+import time
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/description')
+def description():
+    return render_template('description.html')
+
+@app.route('/visualisation')
+def visu():
+    return render_template('visu.html')
+
+@app.route('/runSim')
+def run():
+    return Response(
+        increment(),  # gen_date_time() is an Iterable
+        mimetype='text/event-stream'  # mark as a stream response
+    )
+
+# a generator with yield expression
+def increment():
+    count=0
+    while True:
+        time.sleep(0.005)
+        count+=0.1
+        # DO NOT forget the prefix and suffix
+        yield 'data: %s\n\n' % count
